25b84d72a5a0218c7f95c91ffc880b35e5cd90dc
GodNoden <noe.quezada7446@alumnos.udg.mx>
2026-08-13 05:42:49 +0000

    Fixed imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ThemeToggle from "./components/ThemeToggle";
+import ThemeToggle from "@/components/ui/ThemeToggle"
 import { ThemeProvider } from "next-themes";
 import { LanguageProvider } from "./lib/context";
-import LanguageSelector from "./components/LanguageSelector";
+import LanguageSelector from "@/components/ui/LanguageSelector";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
